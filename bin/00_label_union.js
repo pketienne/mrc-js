c33ad9bf7a0a0901dc7a9ef636a9649a46a2552e
Patrick K. Etienne <12497980+pketienne@users.noreply.github.com>
2015-06-12 17:35:53 -0500

    Set outline of code for bin/00_label_union.js.

---
+++ bin/00_label_union.js
@@ -0,0 +1,32 @@
+/*
+  Read in index.tsv
+  Create array to hold all values from line_number_first_label
+  Create array to hold all values from line_number_last_label
+  Create array to hold the union of values from line_number_first_label and line_number_last_label
+  Write JSON file to capture the contents of the "unioned" array.
+*/
+
+d3.tsv("../tsv/index.tsv", function(data) {
+    var line_number_first_label_values = [];
+    var line_number_last_label_values = [];
+    var values_union = [];
+    
+});
+
+function create_ordinate_array(key) {
+    // get data!
+    var array = [];
+
+    return array;
+}
+
+function unionize_arrays(array1, array2) {
+    // unionize arrays!
+    var array = [];
+
+    return array;
+}
+
+function write_ordinate_array(array) {
+    // write data!
+}
