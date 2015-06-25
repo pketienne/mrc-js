e5b3d3035743f3e220f979632967105223fee01c
Patrick K. Etienne <12497980+pketienne@users.noreply.github.com>
2015-06-25 00:43:43 -0500

    This is bad commit. I've been in the bad habit of using a scratch file to record working code which I then cast aside for casual reference rather than using commits to capture code state. Less of these will occur in the future.

--- js/scratch.js
+++ js/scratch.js
@@ -95,3 +95,104 @@ d3.tsv("tsv/index-updated.tsv", function(data) {
 });
 
 
+d3.tsv("tsv/index-updated.tsv", function(data) {
+    var facts = crossfilter(data);
+
+    var cf_groupAll = facts.groupAll().reduce(
+	function(a,d) {
+	    if(d.fpid in a.fpids) { return a; }
+	    else {a.fpids[d.fpid] = 1; a.numlines += +d.numlines; return a; }
+	},
+	function(a,d) {
+	    a.fpids[d.fpid]--; if(a.fpids[d.fpid] === 0) delete a.fpids[d.fpid]; return a; },
+	function(a,d) { return {'fpids':{}, 'numlines':0} }
+    )
+	console.log(cf_groupAll)
+    var cf_groupAll = facts.groupAll().reduce(
+	function(a,d) {
+	    if(d.fpid in a.fpids) { return a; }
+	    else {a.fpids[d.fpid] = 1; a.numlines += +d.numlines; return a; }
+	},
+	function(a,d) {
+	    a.fpids[d.fpid]--; if(a.fpids[d.fpid] === 0) delete a.fpids[d.fpid]; return a; },
+	function(a,d) { return {'fpids':{}, 'numlines':0} }
+    ).value()
+	console.log(cf_groupAll)
+    var cf_groupAll = facts.groupAll().reduce(
+	function(a,d) {
+	    if(d.fpid in a.fpids) { return a; }
+	    else {a.fpids[d.fpid] = 1; a.numlines += +d.numlines; return a; }
+	},
+	function(a,d) {
+	    a.fpids[d.fpid]--; if(a.fpids[d.fpid] === 0) delete a.fpids[d.fpid]; return a; },
+	function(a,d) { return {'fpids':{}, 'numlines':0} }
+    ).value().numlines;
+	console.log(cf_groupAll)
+
+    var cf_dimension_group = facts.dimension(function(d) { return d.poeta; }).group().
+    console.log(cf_dimension_group)
+    var cf_dimension_group = facts.dimension(function(d) { return d.poeta; }).group().all()
+    console.log(cf_dimension_group)
+
+    var cf_dimension_groupAll = facts.dimension(function(d) { return d.poeta; }).groupAll()
+    console.log(cf_dimension_groupAll)
+    var cf_dimension_groupAll = facts.dimension(function(d) { return d.poeta; }).groupAll().value()
+    console.log(cf_dimension_groupAll)
+
+});
+
+
+
+    
+    var FPID = {
+	add: function(p, v) {
+	    if(v.fpid in p.fpids) {
+		return p;
+	    } else {
+		p.fpids[v.fpid] = 1;
+		p.numlines += +v.numlines;
+		return p;
+	    }
+	},
+	remove: function(p, v) {
+	    p.fpids[v.fpid]--;
+	    if(p.fpids[v.fpid] === 0) {
+		delete p.fpids[v.fpid];
+	    }
+	    return p;
+	},
+	init: function(p, v) {
+	    return { "fpids": {}, "numlines": 0 };
+	};
+    };
+    
+
+    /*
+    var dimensions = {};
+    dimensions["poeta"] = facts.dimension(function(f) { return f.poeta });
+    dimensions["fabula"] = facts.dimension(function(f) { return f.fabula });
+    dimensions["nomen"] = facts.dimension(function(f) { return f.nomen });
+    dimensions["genus_personae"] = facts.dimension(function(f) { return f.genus_personae });
+    dimensions["meter"] = facts.dimension(function(f) { return f.meter });
+    dimensions["metertype"] = facts.dimension(function(f) { return f.metertype });
+    dimensions["meter_before"] = facts.dimension(function(f) { return f.meter_before });
+    dimensions["meter_after"] = facts.dimension(function(f) { return f.meter_after });
+    dimensions["fpid"] = facts.dimension(function(f) { return f.fpid });
+
+    function blah(dimension) {
+	console.log(dimension);
+	console.log(dimension.group());
+	console.log(dimension.group().size());
+    }
+
+    blah(dimensions.poeta);
+    blah(dimensions.fabula);
+    blah(dimensions.nomen);
+    blah(dimensions.genus_personae);
+    blah(dimensions.meter);
+    blah(dimensions.metertype);
+    blah(dimensions.meter_before);
+    blah(dimensions.meter_after);
+    blah(dimensions.fpid);
+    */
+    
