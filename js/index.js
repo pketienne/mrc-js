dd3f7dd0b3f8496bfe806c7e741f14dc71420be4
Patrick K. Etienne <12497980+pketienne@users.noreply.github.com>
2015-07-09 05:17:06 -0500

    Removed old scratch files. Added major functionality across index.js.

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ d3.tsv('tsv/index.tsv', function(data) {
     var Component = function(label) {
 	this.label = label;
 	this.model = new Model(this.label);
-	// this.view = new View(this.label);
+	this.view = new View(this.label, this.model.data);
 	// this.controller = new Controller(this.label);
     }
     
@@ -50,6 +50,63 @@ d3.tsv('tsv/index.tsv', function(data) {
     }
 
 
+    // static class. never instantiated, no constructor.
+    var Transmute = {
+	// for non-nomen fpid flattened models performing LINE_COUNTs
+	type_one: function(selection) {
+	    var transmuted = [];
+	    for(var l = selection.length, i = 0; i < l; ++i) {
+		transmuted.push({ Name: selection[i].key, Lines: selection[i].value.line_count });
+	    }
+	    return transmuted;
+	},
+	// for 'nomen' non-flattened models performing CHARACTER_LINE_COUNTs
+	type_two: function(selection) {
+	    var transmuted = [];
+	    for(var l = selection.length, i = 0; i < l; ++i) {
+		transmuted.push({ Name: selection[i].key, Lines: selection[i].value });
+	    }
+	    return transmuted;
+	},
+	// for the 'raw' model
+	type_three: function(selection) {
+	    var transmuted = [];
+	    for(var l = selection.length, i = 0; i < l; ++i) {
+		transmuted.push({
+		    Poeta: selection[i].poeta,
+		    Fabulae: selection[i].fabulae,
+		    Nomen: selection[i].nomen,
+		    Genera: selection[i].genera,
+		    Start: selection[i].line_number_first_label,
+		    End: selection[i].line_number_last_label,
+		    Lines: selection[i].numlines,
+		    Meter: selection[i].meter,
+		    Meter_Type: selection[i].meter_type,
+		    Meter_Before: selection[i].meter_before,
+		    Meter_After: selection[i].meter_after
+		});
+	    }
+	    return transmuted;
+	},
+	// for the 'refined' model
+	type_four: function(selection) {
+	    var transmuted = [];
+	    for(var l = selection.length, i = 0; i < l; ++i) {
+		// to-do
+	    }
+	    return transmuted;
+	},
+	// for the 'popup' model
+	type_five: function(selection) {
+	    var transmuted = [];
+	    for(var l = selection.length, i = 0; i < l; ++i) {
+		// to-do
+	    }
+	    return transmuted;
+	}
+    }
+
+    
     var Model = function(label) {
 	this.label = label;
 	this.dimension;
@@ -108,6 +165,39 @@ d3.tsv('tsv/index.tsv', function(data) {
 	switch(this.label) {
 	case 'poeta':
 	case 'fabulae':
+	case 'genera':
+	case 'meter':
+	case 'meter_type':
+	case 'meter_before':
+	case 'meter_after':
+	    this.data = Transmute.type_one(this.selection);
+	    break;
+	case 'nomen':
+	    this.data = Transmute.type_two(this.selection);
+	    break;
+	case 'raw':
+	    this.data = Transmute.type_three(this.selection);
+	    break;
+	case 'refined':
+	    this.data = Transmute.type_four(this.selection);
+	    break;
+	case 'popup':
+	    this.data = Transmute.type_five(this.selection);
+	    break;
+	}
+    }
+
+    var View = function(label, data) {
+	this.label = label;
+	this.data = data;
+	this.columns = data[0]; // ?
+	this.status;
+	this.location;
+    }
+    View.prototype.draw = function() {
+	switch(this.label) {
+	case 'poeta':
+	case 'fabulae':
 	case 'nomen':
 	case 'genera':
 	case 'meter':
@@ -115,18 +205,48 @@ d3.tsv('tsv/index.tsv', function(data) {
 	case 'meter_before':
 	case 'meter_after':
 	case 'raw':
+	    this.structure_one();
+	    break;
 	case 'refined':
+	    this.structure_two();
+	    break;
 	case 'popup':
+	    this.structure_three();
+	    break;
 	}
     }
-
-    var View = function(label) {
-	this.label = label;
-	this.status;
-	this.location;
+    View.prototype.structure_one = function() {
+	var table = d3.select(this.name).append('table');
+	var thead = table
+	    .append('thead')
+	    .append('tr')
+	    .classed('label', true)
+	    .selectAll('th')
+	    .data(this.columns)
+	    .enter()
+	    .append('th')
+	    .attr('class', function(d) { return d; })
+	    .html(function(d) { return d; });
+	var tbody = table
+	    .append('tbody');
+	var rows = tbody
+	    .selectAll('tr')
+	    .data(this.data)
+	    .enter()
+	    .append('tr')
+	    .classed('facet', true);
+	var cells = rows
+	    .selectAll('td')
+	    .data(function(row) {
+		return this.columns.map(function(column) {
+		    return {name: column, value: row[column]};
+		});
+	    })
+	    .enter()
+	    .append('td')
+	    .attr('class', function(d) { return d.name; })
+	    .html(function(d) { return d.value; });
     }
-    View.prototype.draw = function() {}
-    View.prototype.structure_one = function() {}
     View.prototype.structure_two = function() {}
     View.prototype.structure_three = function() {}
     
