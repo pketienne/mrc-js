2b49d146d1bfeb2313e433f7cd18e10aed77381f
Patrick K. Etienne <12497980+pketienne@users.noreply.github.com>
2015-07-09 16:16:29 -0500

    Brought refactor up to fully functioning code. Need to address issue with reference-ids for css-to-d3 connectivity.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-var COMPONENT_LABELS = ['poeta', 'fabulae', 'nomen', 'genera',
-			'meter', 'meter_type', 'meter_before',
-			'meter_after', 'raw', 'refined', 'popup']
+var COMPONENT_LABELS = ['poeta', 'fabulae', 'nomen', 'genera', 'meter',
+			'meter_type', 'meter_before', 'meter_after', 'raw',
+			'refined', 'popup']
 var LINE_COUNT = 'numlines';
 var CHARACTER_LINE_COUNT = 'char_numlines';
 var FPID = 'fpid';
@@ -9,12 +9,12 @@ d3.tsv('tsv/index.tsv', function(data) {
     var Crossfilter = crossfilter(data);
     var reference_dimension = Crossfilter
 	.dimension(function(f) { return f['meter_after']; });
-    
+
     var Population = function(crossfilter) {
 	this.components = {};
-	this.populate();
 
-	console.log(this.components);
+	this.populate();
+	this.draw();
     }
     Population.prototype.populate = function() {
 	for(var l = COMPONENT_LABELS.length, i = 0; i < l; ++i) {
@@ -22,100 +22,34 @@ d3.tsv('tsv/index.tsv', function(data) {
 	    this.components[label] = new Component(label);
 	}
     }
-    
-    
+    Population.prototype.draw = function() {
+	for(var property in this.components) {
+	    this.components[property].view.draw();
+	}
+    }
+
     var Component = function(label) {
 	this.label = label;
 	this.model = new Model(this.label);
-	this.view = new View(this.label, this.model.data);
-	// this.controller = new Controller(this.label);
-    }
-    
-    
-    // static class. never instantiated, no constructor.
-    var Reduction = {
-	add: function(p, v) {
-	    if(p.fpids.includes(v[FPID])) {
-		return p;
-	    } else {
-		p.fpids.push(v[FPID]);
-		p.line_count += +v[LINE_COUNT];
-		return p;
-	    }
-	},
-	remove: function(p, v) {},
-	init: function(p, v) {
-	    return { fpids: [], line_count: 0 };
-	}
+	this.view = new View(this.label, this.model.data, this.model.columns);
+	this.controller = new Controller(this.label);
     }
 
-
-    // static class. never instantiated, no constructor.
-    var Transmute = {
-	// for non-nomen fpid flattened models performing LINE_COUNTs
-	type_one: function(selection) {
-	    var transmuted = [];
-	    for(var l = selection.length, i = 0; i < l; ++i) {
-		transmuted.push({ Name: selection[i].key, Lines: selection[i].value.line_count });
-	    }
-	    return transmuted;
-	},
-	// for 'nomen' non-flattened models performing CHARACTER_LINE_COUNTs
-	type_two: function(selection) {
-	    var transmuted = [];
-	    for(var l = selection.length, i = 0; i < l; ++i) {
-		transmuted.push({ Name: selection[i].key, Lines: selection[i].value });
-	    }
-	    return transmuted;
-	},
-	// for the 'raw' model
-	type_three: function(selection) {
-	    var transmuted = [];
-	    for(var l = selection.length, i = 0; i < l; ++i) {
-		transmuted.push({
-		    Poeta: selection[i].poeta,
-		    Fabulae: selection[i].fabulae,
-		    Nomen: selection[i].nomen,
-		    Genera: selection[i].genera,
-		    Start: selection[i].line_number_first_label,
-		    End: selection[i].line_number_last_label,
-		    Lines: selection[i].numlines,
-		    Meter: selection[i].meter,
-		    Meter_Type: selection[i].meter_type,
-		    Meter_Before: selection[i].meter_before,
-		    Meter_After: selection[i].meter_after
-		});
-	    }
-	    return transmuted;
-	},
-	// for the 'refined' model
-	type_four: function(selection) {
-	    var transmuted = [];
-	    for(var l = selection.length, i = 0; i < l; ++i) {
-		// to-do
-	    }
-	    return transmuted;
-	},
-	// for the 'popup' model
-	type_five: function(selection) {
-	    var transmuted = [];
-	    for(var l = selection.length, i = 0; i < l; ++i) {
-		// to-do
-	    }
-	    return transmuted;
-	}
-    }
-
-    
     var Model = function(label) {
 	this.label = label;
 	this.dimension;
 	this.selection;
+	this.transmutation;
 	this.data;
-	
-	this.set_dimension(label /* d3 bug */);
+	this.columns;
+
+	this.set_dimension(label);
 	this.set_selection();
-	this.set_data();
+	this.transmutation = new Transmutation(this.selection);
+	this.set_transmutation();
+	this.data = this.transmutation.data;
+	this.columns = this.transmutation.columns;
+
     }
     Model.prototype.set_dimension = function(label) {
 	switch(this.label) {
@@ -126,7 +60,8 @@ d3.tsv('tsv/index.tsv', function(data) {
 	case 'meter':
 	case 'meter_type':
 	case 'meter_before':
-	    this.dimension = Crossfilter.dimension(function(f) { return f[label]; });
+	    this.dimension = Crossfilter
+		.dimension(function(f) { return f[label]; });
 	    break;
 	case 'meter_after':
 	case 'raw':
@@ -161,7 +96,7 @@ d3.tsv('tsv/index.tsv', function(data) {
 	    break;
 	}
     }
-    Model.prototype.set_data = function() {
+    Model.prototype.set_transmutation = function() {
 	switch(this.label) {
 	case 'poeta':
 	case 'fabulae':
@@ -170,29 +105,113 @@ d3.tsv('tsv/index.tsv', function(data) {
 	case 'meter_type':
 	case 'meter_before':
 	case 'meter_after':
-	    this.data = Transmute.type_one(this.selection);
+	    this.transmutation.type_one();
 	    break;
 	case 'nomen':
-	    this.data = Transmute.type_two(this.selection);
+	    this.transmutation.type_two();
 	    break;
 	case 'raw':
-	    this.data = Transmute.type_three(this.selection);
+	    this.transmutation.type_three();
 	    break;
 	case 'refined':
-	    this.data = Transmute.type_four(this.selection);
+	    this.transmutation.type_four();
 	    break;
 	case 'popup':
-	    this.data = Transmute.type_five(this.selection);
+	    this.transmutation.type_five();
 	    break;
 	}
     }
 
-    var View = function(label, data) {
+    var Transmutation = function(selection) {
+	this.selection = selection;
+	this.data = [];
+	this.columns = [];
+    }
+    Transmutation.prototype.type_one = function() {
+	var selection = this.selection;
+	
+	for(var l = selection.length, i = 0; i < l; ++i) {
+	    this.data.push({
+		Name: selection[i].key, Lines: selection[i].value.line_count
+	    });
+	}
+	this.columns.push('Name', 'Lines');
+    }
+    Transmutation.prototype.type_two = function() {
+	var selection = this.selection;
+	
+	for(var l = selection.length, i = 0; i < l; ++i) {
+	    this.data.push({
+		Name: selection[i].key, Lines: selection[i].value
+	    });
+	}
+	this.columns.push('Name', 'Lines');
+    }
+    Transmutation.prototype.type_three = function() {
+	var selection = this.selection;
+	
+	for(var l = selection.length, i = 0; i < l; ++i) {
+	    this.data.push({
+		Poeta: selection[i].poeta,
+		Fabulae: selection[i].fabulae,
+		Nomen: selection[i].nomen,
+		Genera: selection[i].genera,
+		Start: selection[i].line_number_first_label,
+		End: selection[i].line_number_last_label,
+		Lines: selection[i].numlines,
+		Meter: selection[i].meter,
+		Meter_Type: selection[i].meter_type,
+		Meter_Before: selection[i].meter_before,
+		Meter_After: selection[i].meter_after
+	    });
+	}
+	this.columns.push('Poeta', 'Fabulae', 'Nomen', 'Genera', 'Start', 'End',
+			  'Lines', 'Meter', 'Meter_Type', 'Meter_Before',
+			  'Meter_After');
+    }
+    Transmutation.prototype.type_four = function() {
+	var selection = this.selection;
+	
+	for(var l = selection.length, i = 0; i < l; ++i) {
+	    this.data.push({
+		// TODO
+	    });
+	}
+	this.columns.push('');
+    }
+    Transmutation.prototype.type_five = function() {
+	var selection = this.selection;
+	
+	for(var l = selection.length, i = 0; i < l; ++i) {
+	    this.data.push({
+		// TODO
+	    });
+	}
+	this.columns.push('');
+    }
+
+    var Reduction = {
+	add: function(p, v) {
+	    if(p.fpids.includes(v[FPID])) {
+		return p;
+	    } else {
+		p.fpids.push(v[FPID]);
+		p.line_count += +v[LINE_COUNT];
+		return p;
+	    }
+	},
+	remove: function(p, v) {},
+	init: function(p, v) {
+	    return { fpids: [], line_count: 0 };
+	}
+    }
+
+    var View = function(label, data, columns) {
 	this.label = label;
 	this.data = data;
-	this.columns = data[0]; // ?
-	this.status;
-	this.location;
+	this.columns = columns;
+	this.status = 'on';
+	this.location = "#" + this.label;
     }
     View.prototype.draw = function() {
 	switch(this.label) {
@@ -216,13 +235,21 @@ d3.tsv('tsv/index.tsv', function(data) {
 	}
     }
     View.prototype.structure_one = function() {
-	var table = d3.select(this.name).append('table');
+	var data = this.data;
+	var columns = this.columns;
+	var location = this.location;
+
+	console.log(data);
+	console.log(columns);
+	console.log(location);
+
+	var table = d3.select(location).append('table');
 	var thead = table
 	    .append('thead')
 	    .append('tr')
 	    .classed('label', true)
 	    .selectAll('th')
-	    .data(this.columns)
+	    .data(columns)
 	    .enter()
 	    .append('th')
 	    .attr('class', function(d) { return d; })
@@ -231,14 +258,14 @@ d3.tsv('tsv/index.tsv', function(data) {
 	    .append('tbody');
 	var rows = tbody
 	    .selectAll('tr')
-	    .data(this.data)
+	    .data(data)
 	    .enter()
 	    .append('tr')
 	    .classed('facet', true);
 	var cells = rows
 	    .selectAll('td')
 	    .data(function(row) {
-		return this.columns.map(function(column) {
+		return columns.map(function(column) {
 		    return {name: column, value: row[column]};
 		});
 	    })
@@ -249,8 +276,7 @@ d3.tsv('tsv/index.tsv', function(data) {
     }
     View.prototype.structure_two = function() {}
     View.prototype.structure_three = function() {}
-    
-    
+
     var Controller = function(label) {
 	this.label = label;
 	this.filters = [];
@@ -259,8 +285,6 @@ d3.tsv('tsv/index.tsv', function(data) {
     Controller.prototype.remove = function(filter) {}
     Controller.prototype.reset = function() {}
 
-    
     var population = new Population();
-    
-});
 
+});
