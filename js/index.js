c91048f263a56e849e99c2b886aef518f263449e
Patrick K. Etienne <12497980+pketienne@users.noreply.github.com>
2015-07-14 01:27:19 -0500

    Debugging application of filters and the following process of updating / redrawing components.

--- js/index.js
+++ js/index.js
@@ -1,298 +1,235 @@
-var COMPONENT_LABELS = ['poeta', 'fabulae', 'nomen', 'genera', 'meter',
-			'meter_type', 'meter_before', 'meter_after', 'raw',
-			'refined', 'popup']
+var COMPONENT_LABELS = [
+    'poeta', 'fabulae',// 'nomen', 'genera',
+//    'meter', 'meter_type', 'meter_before', 'meter_after',
+    'raw' /*, 'refined', 'popup'*/
+];
+var DIMENSION_LABELS = [
+    'poeta', 'fabulae', 'nomen', 'genera',
+    'meter', 'meter_type', 'meter_before', 'meter_after'
+];
+var SELECTION_SCHEMAS = {
+    type_one: { 'Name': 'key', 'Lines': [ 'value', 'line_count' ] },
+    type_two: { 'Name': 'key', 'Lines': 'value' },
+    type_three: { 'Poeta': 'poeta', 'Fabulae': 'fabulae', 'Nomen': 'nomen',
+		  'Genera': 'genera', 'Start': 'line_number_first_label',
+		  'End': 'line_number_last_label', 'Lines': 'numlines',
+		  'Meter': 'meter', 'Meter Type': 'meter_type',
+		  'Meter Before': 'meter_before', 'Meter After': 'meter_after' }
+};
 var LINE_COUNT = 'numlines';
 var CHARACTER_LINE_COUNT = 'char_numlines';
 var FPID = 'fpid';
 
-d3.tsv('tsv/index.tsv', function(data) {
-    var Crossfilter = crossfilter(data);
-    var reference_dimension = Crossfilter
-	.dimension(function(f) { return f['meter_after']; });
+var dimensions;
+var population;
 
-    var Population = function(crossfilter) {
-	this.components = {};
+var Population = function() {
+    this.components = {};
 
-	this.populate();
-	this.draw();
-    }
-    Population.prototype.populate = function() {
-	for(var l = COMPONENT_LABELS.length, i = 0; i < l; ++i) {
-	    var label = COMPONENT_LABELS[i];
-	    this.components[label] = new Component(label);
-	}
-    }
-    Population.prototype.draw = function() {
-	for(var property in this.components) {
-	    this.components[property].view.draw();
-	}
+    for( var l = COMPONENT_LABELS.length, i = 0; i < l; ++i ) {
+	var label = COMPONENT_LABELS[ i ];
+	this.components[ label ] = new Component( label );
     }
 
-    var Component = function(label) {
-	this.label = label;
-	this.model = new Model();
-	this.view = new View();
-	this.controller = new Controller();
+    for( var property in this.components ) {
+	this.components[property].update();
     }
-    Component.prototype.populate = function() {
-	this.model.populate(this.label);
+}
 
-    }
-    Compoment.prototype.something = function() {
+var Component = function( label ) {
+    this.label = label;
+    this.model = new Model( label );
+    this.view = new View( label );
+    this.controller = new Controller( label );
+}
+Component.prototype.update = function() {
+    this.view.clear_canvas();
+    this.controller.filter();
+    this.model.transmute();
+    this.view.draw( this.model.data );
+}
+
+var Model = function( label ) {
+    this.label = label;
+    this.dimension;
+    this.selection;
+    this.transmutation;
+    this.data;
+
+    this.populate();
+}
+Model.prototype.populate = function() {
+    var reference_dimension = dimensions[ 'poeta' ];
+    
+    switch( this.label ) {
+    case 'poeta':
+    case 'fabulae':
+    case 'genera':
+    case 'meter':
+    case 'meter_type':
+    case 'meter_before':
+    case 'meter_after':
+	this.dimension = dimensions[ this.label ];
+	this.selection = this.dimension.group()
+	    .reduce( Reduction.add, Reduction.remove, Reduction.init )
+	    .all();
+	this.transmutation = SELECTION_SCHEMAS[ 'type_one' ];
+	break;
+    case 'nomen':
+	this.dimension = dimensions[ this.label ];
+	this.selection = this.dimension.group()
+	    .reduceSum( function( d ) { return d[ CHARACTER_LINE_COUNT ]; })
+	    .all();
+	this.transmutation = SELECTION_SCHEMAS[ 'type_two' ];
+	break;
+    case 'raw':
+	this.dimension = reference_dimension;
+	this.selection = this.dimension.top( 200 ); // SWITCH TO ALL RESULTS
+	this.transmutation = SELECTION_SCHEMAS[ 'type_three' ];
+	break;
 	/*
-	.on("click", function(facet, index) {
-	    this.controller.toggle(facet);
-	});
+    case 'popup':
+	this.dimension = reference_dimension;
+	this.selection = this.dimension.top(Infinity);
+	this.transmutation = DATA_SCHEMAS[ 'type_four' ];
+	break;
 	*/
     }
 
-    var Model = function() {
-	this.label;
-	this.dimension;
-	this.selection;
-	this.transmutation;
-	this.data;
-	this.columns;
-    }
-    Model.prototype.populate = function(label) {
-	this.set_dimension(label);
-	this.set_selection();
-	this.transmutation = new Transmutation(this.selection);
-	this.set_transmutation();
-	this.data = this.transmutation.data;
-	this.columns = this.transmutation.columns;
-    }
-    Model.prototype.set_dimension = function(label) {
-	switch(this.label) {
-	case "poeta":
-	case 'fabulae':
-	case 'nomen':
-	case 'genera':
-	case 'meter':
-	case 'meter_type':
-	case 'meter_before':
-	    this.dimension = Crossfilter
-		.dimension(function(f) { return f[label]; });
-	    break;
-	case 'meter_after':
-	case 'raw':
-	case 'refined':
-	case 'popup':
-	    this.dimension = reference_dimension;
-	    break;
-	}
-    }
-    Model.prototype.set_selection = function() {
-	switch(this.label) {
-	case 'poeta':
-	case 'fabulae':
-	case 'genera':
-	case 'meter':
-	case 'meter_type':
-	case 'meter_before':
-	case 'meter_after':
-	    this.selection = this.dimension.group()
-		.reduce(Reduction.add, Reduction.remove, Reduction.init)
-		.all();
-	    break;
-	case 'nomen':
-	    this.selection = this.dimension.group()
-		.reduceSum(function(d) { return d[CHARACTER_LINE_COUNT]; })
-		.all();
-	    break;
-	case 'raw':
-	case 'refined':
-	case 'popup':
-	    this.selection = this.dimension.top(Infinity);
-	    break;
-	}
-    }
-    Model.prototype.set_transmutation = function() {
-	switch(this.label) {
-	case 'poeta':
-	case 'fabulae':
-	case 'genera':
-	case 'meter':
-	case 'meter_type':
-	case 'meter_before':
-	case 'meter_after':
-	    this.transmutation.type_one();
-	    break;
-	case 'nomen':
-	    this.transmutation.type_two();
-	    break;
-	case 'raw':
-	    this.transmutation.type_three();
-	    break;
-	case 'refined':
-	    this.transmutation.type_four();
-	    break;
-	case 'popup':
-	    this.transmutation.type_five();
-	    break;
-	}
-    }
-
-    var Transmutation = function(selection) {
-	this.selection = selection;
-	this.data = [];
-	this.columns = [];
-    }
-    Transmutation.prototype.type_one = function() {
-	var selection = this.selection;
+    this.transmute();
+}
+Model.prototype.transmute = function() {
+    this.data = [];
+    
+    for( var l = this.selection.length, i = 0; i < l; ++i ) {
+	var packet = {};
 	
-	for(var l = selection.length, i = 0; i < l; ++i) {
-	    this.data.push({
-		Name: selection[i].key, Lines: selection[i].value.line_count
-	    });
-	}
-	this.columns.push('Name', 'Lines');
-    }
-    Transmutation.prototype.type_two = function() {
-	var selection = this.selection;
-	
-	for(var l = selection.length, i = 0; i < l; ++i) {
-	    this.data.push({
-		Name: selection[i].key, Lines: selection[i].value
-	    });
-	}
-	this.columns.push('Name', 'Lines');
-    }
-    Transmutation.prototype.type_three = function() {
-	var selection = this.selection;
-	
-	for(var l = selection.length, i = 0; i < l; ++i) {
-	    this.data.push({
-		Poeta: selection[i].poeta,
-		Fabulae: selection[i].fabulae,
-		Nomen: selection[i].nomen,
-		Genera: selection[i].genera,
-		Start: selection[i].line_number_first_label,
-		End: selection[i].line_number_last_label,
-		Lines: selection[i].numlines,
-		Meter: selection[i].meter,
-		Meter_Type: selection[i].meter_type,
-		Meter_Before: selection[i].meter_before,
-		Meter_After: selection[i].meter_after
-	    });
-	}
-	this.columns.push('Poeta', 'Fabulae', 'Nomen', 'Genera', 'Start', 'End',
-			  'Lines', 'Meter', 'Meter_Type', 'Meter_Before',
-			  'Meter_After');
-    }
-    Transmutation.prototype.type_four = function() {
-	var selection = this.selection;
-	
-	for(var l = selection.length, i = 0; i < l; ++i) {
-	    this.data.push({
-		// TODO
-	    });
-	}
-	this.columns.push('');
-    }
-    Transmutation.prototype.type_five = function() {
-	var selection = this.selection;
-	
-	for(var l = selection.length, i = 0; i < l; ++i) {
-	    this.data.push({
-		// TODO
-	    });
-	}
-	this.columns.push('');
-    }
-
-    var Reduction = {
-	add: function(p, v) {
-	    if(p.fpids.includes(v[FPID])) {
-		return p;
+	for( var property in this.transmutation ) {
+	    if( this.transmutation[ property ] instanceof Array ) {
+		packet[ property ] =
+		    this.selection[ i ]
+		[ this.transmutation[ property ][ 0 ] ]
+		[ this.transmutation[ property ][ 1 ] ];
 	    } else {
-		p.fpids.push(v[FPID]);
-		p.line_count += +v[LINE_COUNT];
-		return p;
+		packet[ property ] = this.selection[ i ]
+		[ this.transmutation[ property ] ];
 	    }
-	},
-	remove: function(p, v) {},
-	init: function(p, v) {
-	    return { fpids: [], line_count: 0 };
 	}
-    }
 
-    var View = function() {
-	this.label;
-	this.status = 'on';
-	this.location = "#" + this.label;
+	this.data.push( packet );
     }
-    View.prototype.draw = function() {
-	switch(this.label) {
-	case 'poeta':
-	case 'fabulae':
-	case 'nomen':
-	case 'genera':
-	case 'meter':
-	case 'meter_type':
-	case 'meter_before':
-	case 'meter_after':
-	case 'raw':
-	    this.structure_one();
-	    break;
-	case 'refined':
-	    this.structure_two();
-	    break;
-	case 'popup':
-	    this.structure_three();
-	    break;
+}
+
+var Reduction = {
+    add: function(p, v) {
+	if(p.fpids.includes(v[FPID])) {
+	    return p;
+	} else {
+	    p.fpids.push(v[FPID]);
+	    p.line_count += +v[LINE_COUNT];
+	    return p;
 	}
+    },
+    remove: function(p, v) {},
+    init: function(p, v) {
+	return { fpids: [], line_count: 0 };
     }
-    View.prototype.structure_one = function() {
-	var data = this.data;
-	var columns = this.columns;
-	var location = this.location;
+}
 
-	var table = d3.select(location).append('table');
-	var thead = table
-	    .append('thead')
-	    .append('tr')
-	    .classed('label', true)
-	    .selectAll('th')
-	    .data(columns)
-	    .enter()
-	    .append('th')
-	    .attr('class', function(d) { return d; })
-	    .html(function(d) { return d; });
-	var tbody = table
-	    .append('tbody');
-	var rows = tbody
-	    .selectAll('tr')
-	    .data(data)
-	    .enter()
-	    .append('tr')
-	    .classed('facet', true)
-	// ".on('click', function() {})" goes here
-	var cells = rows
-	    .selectAll('td')
-	    .data(function(row) {
-		return columns.map(function(column) {
-		    return {name: column, value: row[column]};
-		});
-	    })
-	    .enter()
-	    .append('td')
-	    .attr('class', function(d) { return d.name; })
-	    .html(function(d) { return d.value; });
+var View = function( label ) {
+    this.label = label;
+    this.location = d3.select( '#' + label );
+}
+View.prototype.draw = function( data ) {
+    var columns = Object.keys( data[0] );
+    var location = this.location;
+    var label = this.label;
+
+    var table = this.location.append( 'table' );
+    var thead = table
+	.append( 'thead' )
+	.append( 'tr' )
+	.classed( 'label', true )
+	.selectAll( 'th' )
+	.data( columns )
+	.enter()
+	.append( 'th' )
+	.attr( 'class', function( d ) { return d; } )
+	.html( function( d ) { return d; } );
+    var tbody = table.append( 'tbody' );
+    var rows = tbody
+	.selectAll( 'tr' )
+	.data( data )
+	.enter()
+	.append( 'tr' )
+	.classed( 'facet', true )
+	.on('click', function( facet, index ) {
+	    console.log( facet );
+	    population.components[ label ].controller.toggle( facet.Name );
+	} );
+    var cells = rows
+	.selectAll( 'td' )
+	.data( function( row ) {
+	    return columns.map( function( column ) {
+		return { name: column, value: row[ column ] };
+	    } );
+	} )
+	.enter()
+	.append( 'td' )
+	.attr( 'class', function( d ) { return d.name; } )
+	.html( function( d ) { return d.value; } );
+}
+View.prototype.clear_canvas = function() {
+    this.location.select( 'table' ).remove();
+}
+
+var Controller = function( label ) {
+    this.label = label;
+    this.filters = [];
+}
+Controller.prototype.toggle = function( facet ) {
+    console.log( facet );
+    if( this.filters.includes( facet ) ) {
+	console.log( 'removed' );
+	this.remove( facet );
+    } else {
+	console.log( 'added' );
+	this.add( facet );
     }
-    View.prototype.structure_two = function() {}
-    View.prototype.structure_three = function() {}
-
-    var Controller = function() {
-	this.label;
-	this.filters = [];
+    console.log( this.filters );
+}
+Controller.prototype.add = function( facet ) {
+    this.filters.push( facet );
+    dimensions[ this.label ].filter( facet );
+    this.update();
+}
+Controller.prototype.remove = function( facet ) {
+    var index = this.filters.indexOf( facet );
+    
+    this.filters.splice( index, 1 );
+    dimensions[ this.label ].filterAll();
+    this.update();
+}
+Controller.prototype.filter = function() {
+    for( var l = this.filters.length, i = 0; i < l; ++i ) {
+	dimensions[ this.label ].filter( this.filters[ i ] );
     }
-    Controller.prototype.set_handlers = function() {}
-    Controller.prototype.toggle = function(facet) {}
-    Controller.prototype.add = function(filter) {}
-    Controller.prototype.remove = function(filter) {}
-    Controller.prototype.reset = function() {}
+}
+Controller.prototype.update = function() {
+    population.components[ this.label ].update();
+    population.components[ 'raw' ].update();
+    // population.components[ 'popup' ].update();
+}
 
-    var population = new Population();
+d3.tsv( 'tsv/index.tsv', function( data ) {
+    var Crossfilter = crossfilter( data );
 
+    dimensions = {};
+    for(var l = DIMENSION_LABELS.length, i = 0; i < l; ++i) {
+	var label = DIMENSION_LABELS[ i ];
+	dimensions[ label ] = Crossfilter
+	    .dimension( function( d ) { return d[ label ]; } );
+    }
+
+    population = new Population();
 });
